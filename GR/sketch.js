53f43df22048c5ddfaf6b8a712e5db73d811ba51
Lambert Francis <lfrancis18@punahou.edu>
2020-11-07 15:39:58 -0500

    Commented out code so we can see canvas, added simple slider

--- GR/sketch.js
+++ GR/sketch.js
@@ -1,68 +1,83 @@
 // Setup function is called once
 
 // Global parameters
-var Walls;
-var source;
+// var Walls;
+// var source;
 
 function setup() {
-  // // Create the canvas
-  createCanvas(720, 400);
-  background(200);
 
-  // Create the walls and light source
-  source = lightsource(width / 2, height / 2);
-  Walls = [w1, w2, w3, w4] // Input manually here to desired shape
+  // Create the canvas
+  var canvas = createCanvas(720, 400);
+  canvas.parent('sketch-div');
+  background(32);
+
+
+  var slider = document.getElementById("mass");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+
+
+
+
+  // // Create the walls and light source
+  // source = lightsource(width / 2, height / 2);
+  // Walls = [w1, w2, w3, w4] // Input manually here to desired shape
 
 }
 
 // Draw function is called many times each second
 function draw() {
+  // // FORMAT REFERENCE:
+  // // Point ~ [x0, y1]
+  // // Line ~ [[x0,y0],[x1,y1]] == [point1, point2]
 
-  // FORMAT REFERENCE:
-  // Point ~ [x0, y1]
-  // Line ~ [[x0,y0],[x1,y1]] == [point1, point2]
 
+  // // Initial list of points to draw lines to
+  // points = []
+  // Walls.forEach(wall => {
+  //   wall.forEach(point => {
+  //     points.append([point.x, point.y])
+  //   })
+  // })
 
-  // Initial list of points to draw lines to
-  points = []
-  Walls.forEach(wall => {
-    wall.forEach(point => {
-      points.append([point.x, point.y])
-    })
-  })
+  // // Okay now we have the points we want so if we wanted rays to all the points we could do
+  // // line(point.x, point.y, source.x, source.y) for each point.
+  // // First though we want to remove points s.t. the ray to those points already passes through 
+  // // a wall
 
-  // Okay now we have the points we want so if we wanted rays to all the points we could do
-  // line(point.x, point.y, source.x, source.y) for each point.
-  // First though we want to remove points s.t. the ray to those points already passes through 
-  // a wall
+  // // So lets collect all the lines to make this task easier.
+  // bdrylines = []
+  // Walls.forEach(wall => {
+  //   for (var i = 0; i < wall.length-1; i++) {
+  //     bdrylines.append([wall[i], wall[i+1]]); // Line is stored as two points in array [[x0,y0],[x1,y1]]
+  //   }
+  // })
 
-  // So lets collect all the lines to make this task easier.
-  bdrylines = []
-  Walls.forEach(wall => {
-    for (var i = 0; i < wall.length-1; i++) {
-      bdrylines.append([wall[i], wall[i+1]]); // Line is stored as two points in array [[x0,y0],[x1,y1]]
-    }
-  })
+  // // Ok now lets get the lines from the source to the points
 
-  // Ok now lets get the lines from the source to the points
+  // sourcelines = []
+  // points.forEach(point => {
+  //   sourcelines.append([(source.x, source.y), (point.x, point.y)])
+  // })
 
-  sourcelines = []
-  points.forEach(point => {
-    sourcelines.append([(source.x, source.y), (point.x, point.y)])
-  })
+  // // Great so now lets eliminate the points we dont need by checking if a line to one of 
+  // // the points crosses a wall
 
-  // Great so now lets eliminate the points we dont need by checking if a line to one of 
-  // the points crosses a wall
+  // let rays = [];
+  // sourcelines.forEach(srcline => {
+  //   bdrylines.forEach(bdryline => {
+  //     if (line_intersection(srcline, bdryline) == false) {
+  //       rays.append(rays)
+  //     }
+  //   })
+  // })
 
-  let rays = [];
-  sourcelines.forEach(srcline => {
-    bdrylines.forEach(bdryline => {
-      if (line_intersection(srcline, bdryline) == false) {
-        rays.append(rays)
-      }
-    })
-  })
-
-  // Draw the rays
-  rays.forEach(ray => line(rays[0][0], rays[0][1], rays[1][0], rays[1][1]))
+  // // Draw the rays
+  // rays.forEach(ray => line(rays[0][0], rays[0][1], rays[1][0], rays[1][1]))
 }
